1d750d25984d369a7459903d1b43fc96c0a03ade
sky107 <69970001+sky107@users.noreply.github.com>
2021-01-19 02:54:50 +0530

    Add files via upload

---
+++ React JS/React-04-LiveSearch Pixabay API/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/search.css';
+import Search from "./components/search.js";
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
