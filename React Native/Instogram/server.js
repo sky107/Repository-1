b95e2b564caea3aa92ba872b7a1a371af59c26f9
sky107 <69970001+sky107@users.noreply.github.com>
2021-01-21 16:11:28 +0530

    Update server.js

--- React Native/Instogram/server.js
+++ React Native/Instogram/server.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var con = mysql.createConnection({
-    host: '<hostname>',
-    port: '<portname>',
-    user: '<username>',
-    password: '<password>',
-    database: '<dbname>'
+    host: '<>',
+    port: '<>',
+    user: '<>',
+    password: '<>',
+    database: '<>'
 });
 
 var server = app.listen(4545, function() {
@@ -25,25 +25,75 @@ con.connect(function(error) {
     else console.log("connected");
 });
 
+
+
+/*
+This registratoinx route is to used during user registration 
+you can test by adding parameted after the url
+
+Ex: http://localhost:4545/registrationx.json?regname=Siddharth&regusername=skyscraperasdf84_55s9&regpassword=asfas&regemail=asdijkdstyxcvs@gmail.com&regphone=092542423
+
+*/
+
+app.get('/registrationx.json', function(req, res) {
+
+    var regname = req.query.regname;
+    var regemail = req.query.regemail;
+    var regpassword = req.query.regpassword;
+    var regphone = req.query.regphone;
+    var regusername = req.query.regusername;
+
+
+    con.query(`SELECT * FROM instogramusers where username='${regusername}' or email='${regemail}'`, function(error, rows, fields) {
+        if (error) console.log(error)
+        else {
+            if (rows.length) {
+
+                console.log("User Alreay Exists");
+
+            } else {
+
+                con.query(`INSERT INTO instogramusers(email,name,password,username,phone) VALUES ('${regemail}','${regname}','${regpassword}','${regusername}','${regphone}')`, function(errorx, rowsx, fieldsx) {
+                    if (errorx) console.log(errorx);
+                    else
+                        console.log("User Registartion successful");
+
+                });
+            }
+        }
+        res.send(rows);
+
+    });
+});
+
+
+
+/*
+This users.json route is to used during main login screen
+you can test by adding parameted after the url
+
+Ex: http://localhost:4545/users.json?x=inkers&y=inkers
+
+*/
+
 app.get('/users.json', function(req, res) {
     var user_nameoremail = "";
-        user_nameoremail = req.query.x;
+    user_nameoremail = req.query.x;
     var password = req.query.y;
-	var _name="";
-	console.log(user_nameoremail+password);
+    var _name = "";
     con.query(`SELECT * FROM instogramusers where username='${user_nameoremail}' or email='${user_nameoremail}' and password='${password}'`, function(error, rows, fields) {
         if (error) console.log(error)
         else {
-            if (rows.length){
-                console.log(rows.length);
-		rows.slice(0,1).map(res=>_name=res.Name);
-	con.query(`INSERT INTO instogramlogs(email,name) VALUES ('${user_nameoremail}','${_name}')`,function(error,rows,fields){
-if(error)console.log("Error in Addition"+error);
-else console.log("Log added successfuly");
+            if (rows.length) {
 
-});
-}
-            else
+                console.log("User found: " + user_nameoremail);
+                rows.slice(0, 1).map(res => _name = res.Name);
+                con.query(`INSERT INTO instogramlogs(email,name) VALUES ('${user_nameoremail}','${_name}')`, function(error, rows, fields) {
+                    if (error) console.log("Error in Addition" + error);
+                    else console.log("Log added successfuly");
+
+                });
+            } else
                 console.log("User Not found");
         }
         res.send(rows);
