20d3fb5b15ee0a09254f78864db7274bd57cfafe
sky107 <69970001+sky107@users.noreply.github.com>
2021-02-28 19:02:03 +0530

    Update URLSearchParams-usage.js

--- React JS/Random-stuff/URLSearchParams-usage.js
+++ React JS/Random-stuff/URLSearchParams-usage.js
@@ -15,14 +15,18 @@ for(const [key,value] of usp){
 console.log(`${key} <-=-> ${value}`);
 }
  
-console.log(usp.toString());
+console.log(usp);  /* This will return an object */
+console.log(usp.toString());  /*this will return the required header format by performing conversion */
 
 usp.set('name','Siddharth Kumar Yadav');
 console.log(usp.get('name'));
 
+
+
+/* Method 1 */
 fetch(`search.php?${usp.toString()}`)
 
-
+/* Method 2 */
 fetch('search.php',{method:'post',body:usp})
 .then(res=>console.log(res.json()));
 
