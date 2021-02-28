a185bb6733808b573d4b041f04b81d5c95ea0016
sky107 <69970001+sky107@users.noreply.github.com>
2021-02-28 20:03:40 +0530

    Update index.js

--- Backend-Dev/web-socket-basics/index.js
+++ Backend-Dev/web-socket-basics/index.js
@@ -11,21 +11,13 @@ const wss= new WebSocket.Server({port:4545});
 //when we refresh the page , on each page ws object wss refers to server
 wss.on("connection",ws=>{
 console.log("New client connected");
-
-
-
-
-
-
 ws.on("close",()=>{
 console.log("Client Disconeected");
-
 });
 
 ws.on("message",data=>{
 console.log(`Client has sent us:${(data)}`);
 ws.send(data.toUpperCase());
 });
-
 });
 
