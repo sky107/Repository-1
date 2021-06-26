c118367d2ecfb54e48aafca023133885dbf82057
Siddharth Kumar Yadav <69970001+sky107@users.noreply.github.com>
2021-06-26 23:27:09 +0530

    backhandler in react native

---
+++ React Native/BackHandler.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet, BackHandler, Alert } from 'react-native';
+
+export default function App() {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert('Hold on!', 'Are you sure you want to go back?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        { text: 'YES', onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Click Back button!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
